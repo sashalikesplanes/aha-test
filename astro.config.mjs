5bec04118d8f3ce5f7ee381442b4e3e41452f728
Sasha Kiselev <sdkiselev1812+github@gmail.com>
2024-02-05 18:33:23 +0100

    setup tailwind and implement basic settings page

---
+++ astro.config.mjs
@@ -0,0 +1,7 @@
+import { defineConfig } from 'astro/config';
+import tailwindcss from '@astrojs/tailwind';
+
+// https://astro.build/config
+export default defineConfig({
+  integrations: [tailwindcss()]
+});
